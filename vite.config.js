36e397912c855e31b124b632222264c72f82e0d6
Alejandro-44 <alejandroarguelloduarte@gmail.com>
2024-12-03 21:26:32 -0500

    Agregando configuraciones de acceso en pro

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'), // Usa @ para apuntar a src
     },
   },
+  server: {
+    host: '0.0.0.0', // Permite que sea accesible desde cualquier IP
+    port: process.env.PORT || 3000, // Usa el puerto de entorno o 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5000, // Usa el puerto de entorno o 5000
+  },
 });
